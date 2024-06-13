2107ac50611748bb5336be9dd585997b935146d0
Jorge Juarez <jorgeang33@gmail.com>
2024-06-13 15:58:57 -0600

    🚧 Add get_sender_id and send_text_message methods to Messenger class

--- Messenger.py
+++ Messenger.py
@@ -1,6 +1,7 @@
 import requests
 from SocialMedia import SocialMedia
 
+
 class Messenger(SocialMedia):
     def __init__(self, access_token):
         self._url = "https://graph.facebook.com/v20.0/me/messages"
@@ -16,6 +17,26 @@ class Messenger(SocialMedia):
                 return "Verification token mismatch", 403
             return challenge, 200
         return "Hello world", 200
-    
 
-    
+    def get_sender_id(self, data: dict):
+        try:
+            return data['entry'][0]['messaging'][0]['sender']['id']
+        except (IndexError, KeyError) as e:
+            print(f"Error accessing sender ID: {e}")
+            return None
+
+    def send_text_message(self, sender_id, message):
+        header = {"Content-Type": "application/json",
+                  "Authorization": f"Bearer {self.access_token}"}
+        payload = {
+            "recipient": {
+                "id": sender_id
+            },
+            "messaging_type": "RESPONSE",
+            "message": {
+                "text": message
+            }
+        }
+
+        r = requests.post(self._url, headers=header, json=payload, timeout=10)
+        return r.json()
